8bd100172a5d4f4c017ac88d89ec25e57484a34f
Raphaël Balet <raphael.balet@outlook.com>
2026-06-10 20:02:03 +0200

    build(angular)!: v22

--- projects/ngx-mat-input-tel/src/lib/ngx-mat-input-tel-dialog/ngx-mat-input-tel.dialog.ts
+++ projects/ngx-mat-input-tel/src/lib/ngx-mat-input-tel-dialog/ngx-mat-input-tel.dialog.ts
@@ -1,5 +1,13 @@
 import { KeyValuePipe } from "@angular/common";
-import { Component, ElementRef, Inject, OnInit, ViewChild, computed, signal } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+  computed,
+  signal
+} from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
